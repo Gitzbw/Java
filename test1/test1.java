be19f1fd8f7b9063d43f00c6d8510c6354618011
Gitzbw <81919800@qq.com>
2021-10-15 18:18:53 +0800

    test1

---
+++ test1/test1.java
@@ -0,0 +1,23 @@
+
+/**
+ * @author zbw
+ * @version 1.0
+ */
+
+
+
+public class test1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+	}
+}
+
+class test111
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello test111");
+	}
+}
