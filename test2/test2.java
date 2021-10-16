aebcb40b341c0aaebf65a26fb458a3c2f9a2e5cb
Gitzbw <81919800@qq.com>
2021-10-16 11:50:42 +0800

    test2

---
+++ test2/test2.java
@@ -0,0 +1,13 @@
+/**
+ * @author  zbw
+ * @version  1.0
+ */
+
+public class test2
+{
+	public static void main(String[] args)
+	{
+	   int a =0;
+	   System.out.println("The value of a is " + a );
+	}
+}
