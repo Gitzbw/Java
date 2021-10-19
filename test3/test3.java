25350547a1bce0454b0135fdb65c768265dbe950
Gitzbw <81919800@qq.com>
2021-10-19 13:23:55 +0800

    test3

---
+++ test3/test3.java
@@ -0,0 +1,15 @@
+public class test3
+{
+	public static void main(String[] args)
+	{
+	    char n1 ='a';
+	    char n2 =97;
+	    System.out.println(n1);
+	    System.out.println(n2);
+	    System.out.println((int)n1);
+	    System.out.println('a'+1);
+	    System.out.println(n1+1);
+	    System.out.println((char)(n1+1));
+	    
+	}
+}
