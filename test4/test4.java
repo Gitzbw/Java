11f70736d2f94a3e58139f1b030e358280fd2af4
Gitzbw <81919800@qq.com>
2021-10-20 13:06:20 +0800

    test4

---
+++ test4/test4.java
@@ -0,0 +1,23 @@
+public class test4
+{
+	public static void main(String[] args)
+	{
+		boolean x = true;
+	    if(x)
+	    {
+	    	System.out.println("hello");
+	    	System.out.println(true);
+	    }
+	    String s1 = "123";
+	    System.out.println(s1);
+	    int n = 12;
+	    String s2 = n +"";
+	    System.out.println(s2);
+	    System.out.println(s2+ "");
+	    int i = Integer.parseInt(s1);
+	    System.out.println(i);
+	    System.out.println(s1.charAt(0));
+
+
+	}
+}
