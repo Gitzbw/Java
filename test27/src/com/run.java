481fae31139fa81d085897c0a497da8130364522
Gitzbw <81919800@qq.com>
2021-11-12 13:09:46 +0800

    test27

---
+++ test27/src/com/run.java
@@ -0,0 +1,10 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println("实现一个项目 需要用到分层结构 一个class类作为程序的入口 调用程序的菜单 程序的菜单" +
+                "另开一个类 其中包含菜单和各种方法的调用 方法的调用可以放在另一个类中 对象的创建可以放在另一个类" +
+                "中 方法的调用可以直接创建对象 实现一系列功能");
+    }
+}
+
