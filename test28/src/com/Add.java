915b6321a5f8f6a19f0f9f686e84b8f33946d02f
Gitzbw <81919800@qq.com>
2021-11-13 12:11:32 +0800

    test28

---
+++ test28/src/com/Add.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Add {
+    public static int add(int a,int b)
+    {
+        return a+b;
+    }
+}
---
+++ test28/src/com/run.java
@@ -0,0 +1,9 @@
+package com;
+
+public class run {
+
+    public static void main(String[] args) {
+        System.out.println(Add.add(2,3));
+        System.out.println(Add.add(5,5));
+    }
+}
