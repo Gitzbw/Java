605b9a22d976ace776e52719bebbb75735964df5
Gitzbw <81919800@qq.com>
2021-12-14 12:53:35 +0800

    test42

---
+++ test42/src/com/run.java
@@ -0,0 +1,12 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        boolean pass = true;
+        String s = "123";
+        System.out.println(Integer.parseInt(s));
+        char c = s.charAt(0);
+        System.out.println(c);
+        System.out.println(c+"");
+    }
+}
