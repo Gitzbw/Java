0fa49c96cf3b8d040d3c59a234c4681e7cacfeba
Gitzbw <81919800@qq.com>
2022-03-01 19:49:40 +0800

    test119

---
+++ test119/src/com/run.java
@@ -0,0 +1,9 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        String greeting = "Hello" ;
+        System.out.println(greeting.length());
+        System.out.println(greeting. codePointCount (0 , greeting.length()));
+    }
+}
