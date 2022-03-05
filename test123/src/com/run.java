e932d51067b4bc2310b1661de7d7763f51c960af
Gitzbw <81919800@qq.com>
2022-03-05 13:12:05 +0800

    test123

---
+++ test123/src/com/run.java
@@ -0,0 +1,25 @@
+package com;
+
+import java.util.ArrayList;
+
+public class run {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        ArrayList<int[]> ints = new ArrayList<>();
+    }
+}
+
+abstract class a
+{
+    public void test()
+    {
+
+    }
+
+
+}
+
+class b extends a
+{
+
+}
