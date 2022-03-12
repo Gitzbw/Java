39703669083916e3bea94dec33c258cf29dc5c52
Gitzbw <81919800@qq.com>
2022-03-12 20:09:01 +0800

    test130

---
+++ test130/src/com/run.java
@@ -0,0 +1,28 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        test a = new a();
+
+
+    }
+}
+
+
+class mm <T extends run>
+{
+
+}
+
+interface test
+{
+
+}
+
+class a implements test
+{
+    public void say()
+    {
+        System.out.println("111");
+    }
+}
