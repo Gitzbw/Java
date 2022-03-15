b56ff32bbde0c6d8418641f31c536821e6276cd8
Gitzbw <81919800@qq.com>
2022-03-15 14:20:14 +0800

    test133

---
+++ test133/src/com/run.java
@@ -0,0 +1,19 @@
+package com;
+
+import sun.misc.GC;
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println("静态方法和构造方法不能使用abstract修饰");
+    }
+
+
+}
+
+class a
+{
+    public  static void test()
+    {
+
+    }
+}
