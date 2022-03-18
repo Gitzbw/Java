bcde80cd8ee8c167f0300b2c2015c46108e400d4
Gitzbw <81919800@qq.com>
2022-03-18 12:28:37 +0800

    test136

---
+++ test136/src/com/run.java
@@ -0,0 +1,19 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        int[][] a = new int[1][];
+        int x = 10;
+        float y = 5.0f;
+        String s = "祝你考出好成绩！";
+        System.out.println(s.length());
+        int i = 0;
+        i = ++i;
+        System.out.println(i);
+    }
+}
+
+class a
+{
+
+}
