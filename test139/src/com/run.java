c8048c72bac240b577a408c5f7b9fee092250107
Gitzbw <81919800@qq.com>
2022-03-21 22:18:12 +0800

    test139

---
+++ test139/src/com/run.java
@@ -0,0 +1,38 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        String t = "hello";
+        char c [ ] = {'h','e','l','l','o'};
+        
+
+    }
+
+    public void test()
+    {
+
+    }
+    public void t()
+    {
+        test();
+    }
+
+}
+
+class P
+{
+    private static int test()
+    {
+
+        return 1;
+    }
+
+    protected  void say1()
+    {
+
+    }
+     void say2()
+    {
+
+    }
+}
---
+++ test139/src/test/test.java
@@ -0,0 +1,30 @@
+package test;
+
+import com.run;
+import com.run;
+
+public class test {
+    static{
+        System.out.println("public");
+    }
+}
+
+class S extends run
+{
+
+    void say2()
+    {
+
+    }
+}
+
+class aa
+{
+    static{
+        System.out.println("main");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
