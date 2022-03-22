99240c8c4bbdff5f6f0cc5b7ab50219d4b562dd9
Gitzbw <81919800@qq.com>
2022-03-22 13:04:21 +0800

    test140

---
+++ test140/src/com/run.java
@@ -0,0 +1,32 @@
+package com;
+
+import static java.lang.Float.NaN;
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println(Math.ceil(NaN));
+        System.out.println(Math.floor(-0));
+        long n1 = 123l;
+        float n2 = 123l;
+        
+
+    }
+}
+
+class P
+{
+    public final void test()
+    {
+
+    }
+}
+
+class S extends P
+{
+
+    public  void test(int x)
+    {
+
+    }
+}
+
