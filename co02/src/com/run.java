ead3b2d2536c8876dfb56ef34cb51fbf7772a1d8
Gitzbw <81919800@qq.com>
2022-04-24 12:31:30 +0800

    co02

---
+++ co02/src/com/run.java
@@ -0,0 +1,9 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println("co02");
+        char c = 32;
+        System.out.println(c + "1");
+    }
+}
