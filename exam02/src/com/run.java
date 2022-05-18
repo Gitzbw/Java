7109a190c6784bdb648eea96e44d66d7332cf2bc
Gitzbw <81919800@qq.com>
2022-05-18 23:24:13 +0800

    exam02

---
+++ exam02/src/com/run.java
@@ -0,0 +1,80 @@
+package com;
+
+public class run {
+    public static int mmm = 10;
+    public static int tmp;
+    static class asdk
+    {
+        static
+        {
+            System.out.println("123");
+        }
+    }
+    public static void main(String[] args) {
+        //System.out.println(tmp);
+        int b = a.x;
+        int m = 5;
+        mmm++;
+        new test(1){};
+        run run = new run();
+        run.new x();
+        new p();
+        test test = new test(1);
+        int x = test.x;
+
+
+        class xxx
+        {
+            public void test()
+            {
+                System.out.println(mmm);
+            }
+        }
+    }
+
+    class x
+    {
+        static final int x = 5;
+    }
+    static class p
+    {
+
+    }
+
+
+    public void mmmmmm()
+    {
+        new x();
+    }
+}
+
+
+class test
+{
+    int x;
+
+    public test(int x) {
+        this.x = x;
+    }
+}
+class a
+{
+    static int x = 5;
+
+    static{
+        System.out.println("111");
+    }
+    {
+        System.out.println("222");
+    }
+    public a() {
+        System.out.println("111");
+    }
+}
+
+interface  IA
+{
+
+}
+
+
