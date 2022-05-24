ff9d6de99f7aa9e117ad4268c350dcf1a9683258
Gitzbw <81919800@qq.com>
2022-05-24 14:58:39 +0800

    exam08

---
+++ exam08/src/com/run.java
@@ -0,0 +1,23 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println("exam08");
+        char c = '男';
+        char cc = 2022;
+    }
+}
+
+class test
+{
+    int n1;
+    int n2;
+
+    @Override
+    public String toString() {
+        return "test{" +
+                "n1=" + n1 +
+                ", n2=" + n2 +
+                '}';
+    }
+}
