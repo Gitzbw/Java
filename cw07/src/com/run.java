85c3241720999690debea3cb4fc95181d833c164
Gitzbw <81919800@qq.com>
2022-06-05 11:41:21 +0800

    cw07

---
+++ cw07/src/com/run.java
@@ -0,0 +1,12 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println("cw07");
+        byte b1 = -1;
+        byte b2 = -2;
+        System.out.println(b1+b2);
+        int b3 = b1;
+        System.out.println(b3);
+    }
+}
