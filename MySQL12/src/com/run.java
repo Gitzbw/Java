b16d7d139ba65f22d65607826bba88c29ad3e388
Gitzbw <81919800@qq.com>
2022-07-19 19:10:34 +0800

    MySQL12

---
+++ MySQL12/src/com/run.java
@@ -0,0 +1,17 @@
+package com;
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println("MySQL12");
+
+    }
+}
+
+
+class test
+{
+    class inner
+    {
+
+    }
+}
